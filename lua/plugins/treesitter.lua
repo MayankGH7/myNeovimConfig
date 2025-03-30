14a16de9cda3618d1d6c9f1878b4945e50b87192
MayankGH7 <108425578+MayankGH7@users.noreply.github.com>
2025-03-30 20:23:31 +0530

    add treesitter for lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = {"typescript", "python", "tsx", "markdown", "json"},
+      ensure_installed = {"typescript", "python", "tsx", "markdown", "json", "lua"},
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },  
