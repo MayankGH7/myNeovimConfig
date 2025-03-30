a9ee2b31e9c4fb084bac0da448e23b0dd95e81c7
MayankGH7 <108425578+MayankGH7@users.noreply.github.com>
2025-03-31 02:20:41 +0530

    config of catpuccin theme

--- init.lua
+++ init.lua
@@ -46,8 +46,6 @@ vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts);
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, bufopts)
 
 
-
-
 vim.keymap.set('n', 'j', 'gj', { noremap = true, silent = true })
 vim.keymap.set('n', 'k', 'gk', { noremap = true, silent = true })
 vim.cmd([[colorscheme catppuccin-mocha]])
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
     configs.setup({
       ensure_installed = {"typescript", "python", "tsx", "markdown", "json", "lua"},
       sync_install = false,
-      highlight = { enable = true },
+      highlight = { enable = true, additional_vim_regex_highlighting = false },
       indent = { enable = false },  
     })
   end
