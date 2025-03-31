2fc3acb98c9c586612fb8ee920e98be74010ccfe
MayankGH7 <108425578+MayankGH7@users.noreply.github.com>
2025-03-31 22:46:21 +0530

    add fzf-native support for telescope and make telescope show hidden files in file search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,7 +7,24 @@ return {
     local builtin = require('telescope.builtin')
 
     -- Set up Telescope with default settings
-    telescope.setup{}
+    telescope.setup{
+      extensions = {
+        fzf = {
+          fuzzy = true,                    -- false will only do exact matching
+            override_generic_sorter = true,  -- override the generic sorter
+            override_file_sorter = true,     -- override the file sorter
+            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+          }
+        },
+        pickers = {
+          find_files = {
+            hidden = true
+          }
+        }
+      }
+
+    require('telescope').load_extension('fzf')
 
     -- Key mappings
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
