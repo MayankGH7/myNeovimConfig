b51937f01ae8f6e217e6fd2bbf90d90329dfce18
MayankGH7 <108425578+MayankGH7@users.noreply.github.com>
2025-03-31 09:53:08 +0530

    add copilot and commentry plugin

--- lua/plugins/nvimcmp.lua
+++ lua/plugins/nvimcmp.lua
@@ -30,7 +30,7 @@ return {
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<Tab>'] = cmp.mapping.select_next_item(),
+        -- ['<Tab>'] = cmp.mapping.select_next_item(),
         ['<S-Tab>'] = cmp.mapping.select_prev_item(),
       }),
       sources = cmp.config.sources({
