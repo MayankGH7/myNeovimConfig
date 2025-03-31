e878fcfe1689a94bbc1c80b3c08434e84f5da237
MayankGH7 <108425578+MayankGH7@users.noreply.github.com>
2025-03-31 20:10:04 +0530

    update config of chatcopilot

--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -6,9 +6,18 @@ return {
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
     build = "make tiktoken", -- Only on MacOS or Linux
-    opts = {
-      -- See Configuration section for options
-    },
+    opts = function()
+      local user = vim.env.USER or "User"
+      user = user:sub(1, 1):upper() .. user:sub(2)
+      return {
+        auto_insert_mode = true,
+        question_header = "  " .. user .. " ",
+        answer_header = "  Copilot ",
+        window = {
+          width = 0.4,
+        },
+      }
+    end
     -- See Commands section for default commands if you want to lazy load on them
   },
 }
