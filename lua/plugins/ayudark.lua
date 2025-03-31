57297dd62a1ef87d6021b2e228ae46f08d318994
MayankGH7 <108425578+MayankGH7@users.noreply.github.com>
2025-03-31 11:23:04 +0530

    set ayu dark theme bg for termux and others

--- lua/plugins/ayudark.lua
+++ lua/plugins/ayudark.lua
@@ -1,18 +1,33 @@
 return {
   "Shatur/neovim-ayu",
-  config = function() 
+  config = function()
+    local is_termux = os.getenv("TERMUX_VERSION") ~= nil
+
     require('ayu').setup({
-        overrides = {
-          Normal = { bg = "None" },
-          NormalFloat = { bg = "none" },
-          ColorColumn = { bg = "None" },
-          SignColumn = { bg = "None" },
-          Folded = { bg = "None" },
-          FoldColumn = { bg = "None" },
-          CursorLine = { bg = "None" },
-          CursorColumn = { bg = "None" },
-          VertSplit = { bg = "None" },
-        },
-      })
+
+      overrides = is_termux and {
+        Normal = { bg = "None" },
+        NormalFloat = { bg = "none" },
+        ColorColumn = { bg = "None" },
+        SignColumn = { bg = "None" },
+        Folded = { bg = "None" },
+        FoldColumn = { bg = "None" },
+        CursorLine = { bg = "None" },
+        CursorColumn = { bg = "None" },
+        VertSplit = { bg = "None" },
+      } or {
+        -- Normal = { bg = "#111111" },
+        -- NormalFloat = { bg = "#111111" },
+        -- ColorColumn = { bg = "#111111" },
+        -- SignColumn = { bg = "#111111" },
+        -- Folded = { bg = "#111111" },
+        -- FoldColumn = { bg = "#111111" },
+        -- CursorLine = { bg = "#111111" },
+        -- CursorColumn = { bg = "#111111" },
+        -- VertSplit = { bg = "#111111" },
+      },
+
+    })
   end
 }
+
