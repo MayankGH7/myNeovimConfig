d834f3c2513e772098c146a150710fcc3255760d
MayankGH7 <108425578+MayankGH7@users.noreply.github.com>
2025-03-31 20:06:03 +0530

    add lsp error indicator in bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -9,13 +9,14 @@ return {
           return string.format('%s', opts.ordinal)
         end,
         close_command = "bdelete! %d",
+        diagnostics = "nvim_lsp",
         right_mouse_command = "bdelete! %d",
         left_mouse_command = "buffer %d",
         middle_mouse_command = nil,
         indicator_icon = '▎',
         buffer_close_icon = '',
         modified_icon = '●',
-        close_icon = '',
+        close_icon = 'X',
         left_trunc_marker = '',
         right_trunc_marker = '',
         name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
