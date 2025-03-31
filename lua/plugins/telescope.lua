f954e4042161b3fe1a39fab08248bd06de6df95b
MayankGH7 <108425578+MayankGH7@users.noreply.github.com>
2025-03-31 23:02:06 +0530

    add fzf-lua plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,20 @@
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = { 
+    'nvim-lua/plenary.nvim',
+    'ibhagwan/fzf-lua',
+  },
   config = function()
     local telescope = require('telescope')
     local builtin = require('telescope.builtin')
+    local fzf = require('fzf-lua')
 
-    -- Set up Telescope with default settings
+
     telescope.setup{
+      defaults = {
+        file_ignore_patterns = { "%.git/" },
+      },
       extensions = {
         fzf = {
           fuzzy = true,                    -- false will only do exact matching
@@ -21,17 +28,23 @@ return {
           find_files = {
             hidden = true
           }
-        }
+        },
       }
 
-    require('telescope').load_extension('fzf')
+      require('telescope').load_extension('fzf')
 
-    -- Key mappings
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-    vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, { desc = 'Telescope fuzzy find in current buffer' })
-  end
-}
+      -- Key mappings
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, { desc = 'Telescope fuzzy find in current buffer' })
+
+      -- Key mappings for fzf-lua (you can adjust these according to your preference)
+      vim.keymap.set('n', '<leader>fl', fzf.files, { desc = 'fzf-lua find files' })
+      vim.keymap.set('n', '<leader>fg', fzf.grep, { desc = 'fzf-lua live grep' })
+      vim.keymap.set('n', '<leader>fb', fzf.buffers, { desc = 'fzf-lua buffers' })
+      vim.keymap.set('n', '<leader>fk', fzf.builtin, { desc = 'fzf-lua builtin commands' })
+    end
+  }
 
