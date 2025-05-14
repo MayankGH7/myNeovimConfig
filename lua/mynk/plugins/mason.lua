2b394a901e271e454410f7123507829e18dc6694
MayankGH7 <108425578+MayankGH7@users.noreply.github.com>
2025-05-14 19:36:23 +0530

    add mason-lspcofig plugin and add some ensure installed plugin lsp servers and automatically set them

--- lua/mynk/plugins/mason.lua
+++ lua/mynk/plugins/mason.lua
@@ -1,6 +1,10 @@
 return {
-  "williamboman/mason.nvim",
-  config = function() 
+  "mason-org/mason.nvim",
+  dependencies = {
+    "mason-org/mason-lspconfig.nvim",
+  },
+  config = function()
+
     require'mason'.setup({
       ui = {
         icons = {
@@ -10,5 +14,20 @@ return {
         }
       }
     })
+
+    require'mason-lspconfig'.setup({
+      automatic_enable = true,
+      ensure_installed = {
+        "lua_ls",
+        "gopls",
+        "clangd",
+        "pyright",
+        "ts_ls",
+        "cssls",
+        "emmet_language_server",
+        "jsonls"
+      },
+    })
+
   end,
 }
