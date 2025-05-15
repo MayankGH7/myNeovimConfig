a3b1ee4207a76be85645d8da2e086390dc39cb26
MayankGH7 <108425578+MayankGH7@users.noreply.github.com>
2025-05-15 08:56:06 +0530

    make certain lsp not install in termux env

--- lua/mynk/plugins/mason.lua
+++ lua/mynk/plugins/mason.lua
@@ -14,20 +14,25 @@ return {
       }
     })
 
-    require 'mason-lspconfig'.setup({
-      automatic_enable = false,
-      ensure_installed = {
+    local servers = {
         "lua_ls",
         "gopls",
-        "clangd",
         "ts_ls",
         "cssls",
         "emmet_language_server",
         "jsonls",
-        "ruff",
-        "pyright",
-      },
+        "pyright"
+      }
+    local is_termux = os.getenv("TERMUX_VERSION")
 
+    if not is_termux then
+      table.insert(servers, "ruff")
+      table.insert(servers, "clangd")
+    end
+
+    require 'mason-lspconfig'.setup({
+      automatic_enable = false,
+      ensure_installed = servers,
     })
   end,
 }
